5b2b35d9dc5af97b5c7a65b11a237f24c9b00838
hector-munachi <victornothector@gmail.com>
2023-04-21 15:33:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Slr from './components/Slr'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex h-screen justify-center items-center"> 
+        <Slr />
+      </div>
+    </>
+  )
+}
+
+export default App
